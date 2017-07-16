b6280c7d19d20bd362443588c57b9fcd9cbb2772
greatd4n3 <danezcrum@gmail.com>
2017-07-15 23:43:49 -0400

    Create gen7.py
    Main spider function. Run this file using 'scrapy runspider gen7.py' from the working directory. 
    The purpose of this python file is to yield items from an ebay page and ultimately parse the output to a csv file.

---
+++ gen7.py
@@ -0,0 +1,36 @@
+import scrapy 
+
+class quotescraper(scrapy.Spider):
+	name = 'gen5'
+	start_urls = ['https://www.ebay.com/sch/areatrend/m.html?_nkw&_armrs=1&_ipg&_from&LH_BIN=1&rt=nc&_udlo&_udhi&LH_PrefLoc=98&LH_Sold=1&LH_Complete=1']
+	CONCURRENT_ITEMS = 4
+	CONCURRENT_REQUESTS = 4
+# above url can be changed to ebay page you want to use. Should work on any sellers page with the options 'buy it now + sold' selected.
+
+	def parse(self,response):
+		for title in response.xpath('//div[@id="ResultSetItems"]'):
+			link = {
+			'link':title.xpath('//h3/a[@class="vip"]/@href').extract_first(), 
+			#'title':title.xpath('//a[@class="vip"]//text('')').extract(),
+			#'price':title.xpath('normalize-space(//span[@class="bold bidsold"]//text())').extract(), 
+			#'listprice':title.xpath('//span[@class="stk-thr"]//text()').extract(),
+					}
+
+#	yield link
+		for i in response.xpath('//div[@id="ResultSetItems"]'):
+			title = {
+			'title':i.xpath('//a[@class="vip"]//text('')').extract_first(),
+								}
+	#	yield title 
+		for i in response.xpath('//div[@id="ResultSetItems"]'):
+			price = {
+			'price':i.xpath('normalize-space(//span[@class="bold bidsold"]//text())').extract_first(),
+			}
+
+		yield link
+		yield title
+		yield price
+
+# trying to clean the output for multiple items. 
+# attempted to create individual for loops and yield individual output
+# Not working well. 
