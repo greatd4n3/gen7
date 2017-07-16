7ba5997d121add64f99264b689a74123a39459f8
greatd4n3 <danezcrum@gmail.com>
2017-07-15 23:45:35 -0400

    Update gen7.py
    Updated comments, cleaned up code.

--- gen7.py
+++ gen7.py
@@ -6,6 +6,7 @@ class quotescraper(scrapy.Spider):
 	CONCURRENT_ITEMS = 4
 	CONCURRENT_REQUESTS = 4
 # above url can be changed to ebay page you want to use. Should work on any sellers page with the options 'buy it now + sold' selected.
+# concurrent items/requests was set in an attempt to simplify the output. this has not worked as intended. 
 
 	def parse(self,response):
 		for title in response.xpath('//div[@id="ResultSetItems"]'):
@@ -14,14 +15,14 @@ class quotescraper(scrapy.Spider):
 			#'title':title.xpath('//a[@class="vip"]//text('')').extract(),
 			#'price':title.xpath('normalize-space(//span[@class="bold bidsold"]//text())').extract(), 
 			#'listprice':title.xpath('//span[@class="stk-thr"]//text()').extract(),
-					}
+			}
 
-#	yield link
+#yield title
 		for i in response.xpath('//div[@id="ResultSetItems"]'):
 			title = {
 			'title':i.xpath('//a[@class="vip"]//text('')').extract_first(),
-								}
-	#	yield title 
+			}
+#yield price 
 		for i in response.xpath('//div[@id="ResultSetItems"]'):
 			price = {
 			'price':i.xpath('normalize-space(//span[@class="bold bidsold"]//text())').extract_first(),
